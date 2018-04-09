9e7171a1619af3914aa0e74de9da9199fa142cc8
Addison Webb <addisonwebb@icloud.com>
2018-04-09 08:43:51 -0500

    add project

---
+++ NashvilleZaTests/MarchCodingChallengeTests.swift
@@ -0,0 +1,20 @@
+//
+//  MarchCodingChallengeTests.swift
+//  NashvilleZaTests
+//
+//  Created by Addison Webb on 4/9/18.
+//  Copyright © 2018 Nashville CocoaHeads. All rights reserved.
+//
+
+import XCTest
+
+class AprilCodingChallengeTests: XCTestCase {
+    
+    /// Challenge 2
+    func testJSONDecoding() {
+        // Verify that our code can successfully decode the JSON
+        // Hint: This might need to be tested with more than one test as there might be more
+        // than one method used in the process of decoding the JSON
+    }
+    
+}

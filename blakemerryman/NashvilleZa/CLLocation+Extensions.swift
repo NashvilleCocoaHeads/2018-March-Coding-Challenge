e383f0f904a868af16f7ddcdc3546a34018bd90f
Blake Merryman <blake.merryman@gmail.com>
2018-04-10 09:04:46 -0500

    Create blakemerryman solution directory

---
+++ blakemerryman/NashvilleZa/CLLocation+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  CLLocation+Extensions.swift
+//  NashvilleZa
+//
+//  Created by Addison Webb on 3/28/18.
+//  Copyright © 2018 Nashville CocoaHeads. All rights reserved.
+//
+
+import CoreLocation
+
+extension CLLocationDistance {
+    var miles: Double {
+        return self * 0.000621371
+    }
+    
+    /// Challenge 1:
+    /// This method is currently tested by `testMilesString()`.
+    /// Update this method to make testing easier. For a hint see `testMilesString()` in `NashvilleZaTests.swift`.
+    var milesString: String {
+        return String(format: "%.2f miles", miles)
+    }
+}
